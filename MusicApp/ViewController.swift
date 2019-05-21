9c873377acb736119c5ad1a830b96ede65902308
theeinsaneone <adhall1811@gmail.com>
2019-05-22 00:37:25 +0530

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Abhishek Dhall on 22/05/19.
+//  Copyright © 2019 Abhishek Dhall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
